cb6badd315e91cf3ffda2ff369ce0c6997431fa9
zhangyong <zhangyong7120180@163.com>
2021-10-23 19:07:42 +0800

    [添加代码] 添加smart mq

---
+++ smart-mq/smart-mq-client/src/main/java/com/courage/smart/mq/client/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by zhangyong on 2021/10/23.
+ */
+package com.courage.smart.mq.client;
---
+++ smart-mq/smart-mq-server/src/main/java/com/courage/smart/mq/server/MainApplication.java
@@ -0,0 +1,11 @@
+package com.courage.smart.mq.server;
+
+/**
+ * 
+ * Created by zhangyong on 2021/10/23.
+ */
+public class MainApplication {
+
+
+
+}
